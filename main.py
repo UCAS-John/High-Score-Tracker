0068c4476ba0b7617516cda1ac9ae189e3b8c9c1
AlishyaX <alishya.xavier@ucas-edu.net>
2025-02-24 13:44:26 -0700

    finished game.py and started main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from game import play_game
 from high_score import *
 
 def main():
+    choice = input('What would you like to do?\n1. Play Game\n2. Display top 10 scores\n3. Display all scores')
     raise NotImplementedError
 
 if __name__ == "__main__":
