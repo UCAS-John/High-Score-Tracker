b4376778dd38a0197c0a00922407fccdad39874d
UCAS-John <john.wangwang@ucas-edu.net>
2025-02-24 13:44:09 -0700

    make load and update functions

--- high_score.py
+++ high_score.py
@@ -8,12 +8,31 @@ def check_file():
 
 # John
 def load():
-    raise NotImplementedError
+    high_score = {}
+    try:
+        with open(PATH, "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                high_score[row["name"]] = row["score"]
+    except Exception as e:
+        print(f"Error loading: {e}")
+        return None
+    
+    return high_score
 
 # John
 def update():
-    raise NotImplementedError
-
+    high_score = load()
+    fieldnames = ["name", "score"]
+    try:
+        with open(PATH, "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow
+    except Exception as e:
+        print(f"Error update high score: {e}")
+        return False
+    
 #Eli
 def display_score():
     raise NotImplementedError
