f0d428a8a9076f7dd722676b706a797854a13c58
UCAS-John <john.wangwang@ucas-edu.net>
2025-02-25 13:38:45 -0700

    load and update function

--- high_score.py
+++ high_score.py
@@ -7,36 +7,45 @@ def check_file():
     raise NotImplementedError
 
 # John
-def load():
+def load() -> dict:
     high_score = {}
+    my_list = []
     try:
-        with open(PATH, "r") as file:
-            reader = csv.DictReader(file)
+        with open(PATH, newline="") as file:
+            reader = csv.reader(file, delimiter=",")
+            if reader is None:
+                return high_score
             for row in reader:
-                high_score[row["name"]] = row["score"]
+                my_list.append(row)
+            if len(my_list) != 2:
+                return high_score
+            for key, value in zip(my_list[0], my_list[1]):
+                high_score[key] = value
     except Exception as e:
         print(f"Error loading: {e}")
-        return None
-    
+
     return high_score
 
 # John
-def update():
+def update(name: str, score: int) -> None:
+    high_score = {}
     high_score = load()
-    fieldnames = ["name", "score"]
+    high_score[name] = score
+
     try:
         with open(PATH, "w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow
+            writer = csv.writer(file)
+            writer.writerow(high_score.keys())
+            writer.writerow(high_score.values())
     except Exception as e:
-        print(f"Error update high score: {e}")
-        return False
-    
+        print(f"Error updating: {e}")
+
 #Eli
 def display_score():
     raise NotImplementedError
 
 # Use this to test your function
 if __name__  == "__main__":
-    pass
+    update("John", 20)
+    update("Anna", 80)
+    update("John", 90)
