0a66f8562c31496ec571b146f64a18dc9559871b
REli255 <eli.robison@ucas-edu.net>
2025-02-25 13:14:51 -0700

    added display function

--- high_score.py
+++ high_score.py
@@ -2,6 +2,12 @@ import csv
 
 PATH = "high_score.csv"
 
+scores = []
+
+with open("high_score.csv", "r") as file:
+    for row in file:
+        scores.append(row)
+
 # Locklin
 def check_file():
     raise NotImplementedError
@@ -15,8 +21,13 @@ def update():
     raise NotImplementedError
 
 #Eli
-def display_score():
-    raise NotImplementedError
+def display_top_ten():
+    top = 10
+    for score in scores:
+        if top <= 0:
+            break
+        print(score)
+        top -= 1
 
 # Use this to test your function
 if __name__  == "__main__":
