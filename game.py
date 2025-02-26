4c2517ab013ac5ac6f781b70b5bb72dd1983f04a
AlishyaX <alishya.xavier@ucas-edu.net>
2025-02-26 13:44:52 -0700

    edited play_game() function

--- game.py
+++ game.py
@@ -1,46 +1,59 @@
 # Alishya 
 
+#This helps the computer randomly pick one of the choices
 import random
+
+#This is the function that plays the rock paper scissors game 
 def play_game():
+
+    #These take care of the players score
     score = 0
-    comp_score = 0
+
+    time = 0
     while True:
+
         computer = ['rock', 'paper', 'scissors']
         user = input('Type in your choice(rock, paper, or scissors): ')
         computer_choice = random.choice(computer)
         print('The computer chose .......\n', computer_choice)
         if computer_choice == 'rock' and user == 'rock':
-            print('This was a tie')
+            print('This was a tie.(No points are given)')
+            time+=1
         elif computer_choice == 'rock' and user == 'paper':
             print('You won this round')
             score+=1
+            time+=1
         elif computer_choice == 'rock' and user == 'scissors':
             print('The computer won this round')
-            comp_score+=1
+            time+=1
         elif computer_choice == 'paper' and user == 'paper':
-            print('This was a tie')
+            print('This was a tie.(No points are given)')
+            time+=1
         elif computer_choice == 'paper' and user == 'rock':
             print('The computer won this round')
-            comp_score+=1
+            time+=1
         elif computer_choice == 'paper' and user == 'scissors':
             print('You won this round.')
             score+=1
+            time+=1
         elif computer_choice == 'scissors' and user == 'scissors':
-            print('This is a tie.')
+            print('This is a tie.(No points are given)')
+            time+=1
         elif computer_choice == 'scissors' and user == 'paper':
             print('The computer won this round')
-            comp_score+=1
+            time+=1
         elif computer_choice == 'scissors' and user == 'rock':
-            print('You won this round,')
+            print('You won this round.')
             score+=1
+            time+=1
         else:
-            print('That is not one of the options')
+            print('That is not one of the options. Try again...')
+            continue
         
         choice = input('Would you like to exit the game? y/n\n')
         if choice == 'y':
-            complete_score = ('Your final score is: ',score)
-            comp_complete_score = ('The computers final score is:', comp_score)
-            print(complete_score,'\n',comp_complete_score )
+            complete_score = ('Your final score is',score,'out of',time,'attempts.')
+            print(complete_score)
         else:
             continue
 
