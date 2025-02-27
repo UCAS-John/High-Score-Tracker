732e739b63f4abe1d4975b3efa3f083b457bd772
UCAS-John <john.wangwang@ucas-edu.net>
2025-02-27 13:24:05 -0700

    Make sign up automatical login

--- user.py
+++ user.py
@@ -19,3 +19,32 @@ def check_user(username: str, password: str) -> bool:
         if password == users[username]:
             return True
     return False
+
+if __name__ == "__main__":
+
+    while True:
+        log_in_or_exit = input('What do you want to do?\n1. Log into an account\n2. Sign up\n3. Exit\nChoice: ').strip()
+
+        match log_in_or_exit:
+            case '1':
+                username = input('What is your username: ').strip()
+                password = input('What is your password: ').strip()
+                if check_user(username, password):
+                    print(f"\nWelcome {username}\n")
+                    break
+                else:
+                    print("\nInvalid Username or Password\n")
+                    continue
+            case '2':
+                username = input('What is your username: ').strip()
+                password = input('What is your password: ').strip()
+                store_user(username, password)
+                continue
+
+            case '3':
+                print('Thankyou for playing!')
+                break
+
+            case _:
+                print('That is not an option. Try again')
+                continue
